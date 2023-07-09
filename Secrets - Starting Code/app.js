a146810be6604ad9b9ff93e972889c5fd7069b87
Karin Ochayon <karinoch070@gmail.com>
2023-07-09 12:52:12 +0300

    Salting & Hashing

--- Secrets - Starting Code/app.js
+++ Secrets - Starting Code/app.js
@@ -3,8 +3,11 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
-
+// const encrypt = require("mongoose-encryption");
+// const md5 = require("md5");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
+ 
 const app = express();
 
 console.log(process.env.SECRET);
@@ -20,7 +23,7 @@ const userSchema = new mongoose.Schema({
     password: String
 });
 
-userSchema.plugin(encrypt, {secret :  process.env.SECRET, encryptedFields: ["password"]});
+// userSchema.plugin(encrypt, {secret :  process.env.SECRET, encryptedFields: ["password"]});
 
 const User = new mongoose.model("User", userSchema);
 
@@ -39,18 +42,22 @@ app.get("/register", function(req, res){
 
 app.post("/register", function(req, res){
 
-    const newUser = new User({
-        email: req.body.username,
-        password: req.body.password
-    })
+    bcrypt.hash(req.body.password, saltRounds, function(err, hash){
 
-    newUser.save()
-    .then(function(){
-        res.render("secrets");
-    })
-    .catch(function(err){
-        console.log(err);
+        const newUser = new User({
+            email: req.body.username,
+            password: hash
+        })
+    
+        newUser.save()
+        .then(function(){
+            res.render("secrets");
+        })
+        .catch(function(err){
+            console.log(err);
+        });
     });
+
 });
 
 app.post("/login", function(req, res){
